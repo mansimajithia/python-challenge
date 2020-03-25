ab0c0d7ee971e2615c54e79aef9adcb14326358e
Mansi Majithia <mansi@Mansis-MacBook-Pro.local>
2020-03-25 11:37:02 -0500

    added homework files

---
+++ PyBank/main.py
@@ -0,0 +1,49 @@
+#Modules
+import os
+import csv
+
+#Set path for file
+csvpath ='../Resources/budget_data.csv'
+
+#Set Variables
+months = []
+total_revenue = []
+greatest_increase = []
+greatest_decrease = []
+revenue = 0
+
+#Read in the csv file
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csvheader}")
+    # Count Total # of months and total net amount
+    for row in csvreader: 
+        months.append(row[0])
+        total_revenue.append(int(row[1]))
+#Total months = length of months
+#total revenue = length of revenue
+print(len(months))
+print(len(total_revenue))
+
+#Loop through to compare profit/losses to previous row and set revenue to 0
+total_months = len(months)
+total_revenue = len(revenue)
+average_change = (total_revenue/total_months)
+revenue = 0
+for x in range(len(total_revenue)):
+    if revenue >= greatest_increase
+        greatest_increase = revenue
+        month_increase = months
+    elif revenue <= greatest_decrease
+        greatest_decreae = revenue
+        month_decrease = months
+
+        #Print the Following Information
+Print("Financial Analysis")
+Print("---------------------------")
+Print(f"Total Months: + {total_months}")
+Print(f"Total + {int(total_revenue)}")
+Print(f"Greatest Increase in Profits: + {month_increase} + ({greatest_increase})")
+Print(f"Greatest Decrease in Profits: + {month_decrease} + ({greatest_decrease})")
