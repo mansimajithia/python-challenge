cf7569d517a52a0b6fb8c2c8a1c3a2eecdddd025
Mansi Majithia <mansi@Mansis-MacBook-Pro.local>
2020-03-27 18:18:00 -0500

    finished both hws

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,48 +19,50 @@ row = []
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
-    # csv_header = next(csvreader)
+    csv_header = next(csvreader)
     # print(f"CSV Header: {csv_header}")
     for row in csvreader: 
         months.append(row[0])
-        total_revenue.append(row[1])
+        total_revenue.append(int(row[1]))
         monthly_rev_change = int(row[1])- previous_revenue
         revenue_change.append(monthly_rev_change)
         if (monthly_rev_change > greatest_increase):
             greatest_increase_month = row [0]
             greatest_increase = monthly_rev_change
         elif (monthly_rev_change < greatest_decrease):
-            greatest_decrease_month = row [0]
-            greatest_decrease = monthly_rev_change
+             greatest_decrease_month = row [0]
+             greatest_decrease = monthly_rev_change
         previous_revenue = int(row[1])
 print(greatest_increase_month)
+# print(total_revenue)
 print(sum(total_revenue))
 total_months = len(months)
 print(total_months)
 print(greatest_increase)
 print(greatest_decrease)
 print(greatest_decrease_month)
+total_revenue_change =(sum(revenue_change))
+average_change = (total_revenue_change/(total_months))
+print(average_change)
 
 #Print Financial Analysis
 print("Financial Analysis")
 print("---------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total: ${final_total_revenue}")
-print(f"Average Change: ${average}")
+print(f"Total Months: {len(months)}")
+print(f"Total: ${sum(total_revenue)}")
+print(f"Average Change: ${average_change}")
 print(f"Greatest Increase in Profits: {greatest_increase_month} $({greatest_increase})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_month} $({greatest_decrease})")
 
 #Export to text file
-outpath = os.path.join("..","output", "budget_data.txt")
-with open(outputpath, 'w') as outputfile:
-    textwriter = textwriter(textfile)
+# "budget_data.txt"
+with open("budget_data.txt", 'w') as file:
+    # textwriter = textwriter(textfile)
 
-    outputfile.write("Financial Analysis\n")
-    outputfile.write("---------------------------\n")
-    outputfile.write(f"Total Months: + {total_months}\n")
-    outputfile.write(f"Total + {int(total_revenue)}\n")
-    outputfile.write(f"Greatest Increase in Profits: + {month_increase} + ({greatest_increase})\n")
-    outputfile.write(f"Greatest Decrease in Profits: + {month_decrease} + ({greatest_decrease})")
-
-for lines in line:
-    print(line)
+    file.write("Financial Analysis\n")
+    file.write("---------------------------\n")
+    file.write(f"Total Months: {total_months}\n")
+    file.write(f"Total + ${sum(total_revenue)}\n")
+    file.write(f"Average Change: ${average_change}\n")
+    file.write(f"Greatest Increase in Profits: + {greatest_increase_month} + ({greatest_increase})\n")
+    file.write(f"Greatest Decrease in Profits: + {greatest_decrease_month} + ({greatest_decrease})")
